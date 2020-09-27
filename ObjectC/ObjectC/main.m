5149370987d9843f570e2d7839df35ffbbab5ed0
taixin.jiang <taixin.jiang@pfizer.com>
2020-09-27 14:53:10 +0800

    create project
    create project

---
+++ ObjectC/ObjectC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectC
+//
+//  Created by taixin.jiang on 2020/9/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
